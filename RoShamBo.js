8e8407be078fbbbd50af5528723ae841f8255235
sirshakespeare25 <sirshakespeare25@gmail.com>
2022-08-07 23:52:57 -0500

    First Project milestone complete, moving on to next lesson

--- RoShamBo.js
+++ RoShamBo.js
@@ -1,4 +1,5 @@
 const test = "hello World!";
+let score = 0;
 
 
 
@@ -19,33 +20,56 @@ function getPlayerChoice(playerResponse = prompt("Rock, Paper, Scissors! Make yo
         return playerResponse;
      }
     return playerChoice;
-}
+};
 
 function correctString(word) {
     let a = (word.slice(0,1));
         b = (word.slice(1));
         finalWord = (a.toUpperCase()+b.toLowerCase());
     return finalWord;
-}
+};
 
 
 function playRound() {
     const computerChoice = getComputerChoice();
     const playerChoice = getPlayerChoice();
     if (playerChoice == computerChoice) {
-        alert(playerChoice + ' is the same as ' + computerChoice)
+        alert(playerChoice + ' is the same as ' + computerChoice);
+        playRound();
 }
 
     else if ((playerChoice === "Rock" && computerChoice === 'Scissors') || (playerChoice === 'Paper' && computerChoice === 'Rock') || (playerChoice === 'Scissors' && computerChoice === 'Paper')) {
         alert("You win! " + (playerChoice) + " beats " + (computerChoice) + "!");
+        return true;
 }
 
     else {
-        alert("You lose!" + (computerChoice) + " beats " + (playerChoice) + "!")
+        alert("You lose! " + (computerChoice) + " beats " + (playerChoice) + "!");
+        return false;
 }
-}
+};
 
+function playGame() {
+    for (i = 0; i < 5; i++) {
+        if (playRound() === true) {
+            score++
+        }
+        else {
+            score--
+        }
+    };
+    
+    scoreBoard(score);
+};
 
+function scoreBoard(finalScore) {
+    if (Number(finalScore) > 0) {
+        alert("You won this Time!")
+    }
 
+    else {
+        alert("Better luck next Time!")
+    }
+};
 
-;
+playGame();
